1f8a9d3d431f893e28c09a1855522153b80e781c
aragas <va_mikhaylov@head-point.ru>
2021-09-09 11:55:16 +0300

    Respect Retry-After

--- src/NexusMods.Monitor.Shared.Host/PollyUtils.cs
+++ src/NexusMods.Monitor.Shared.Host/PollyUtils.cs
@@ -12,16 +12,36 @@ namespace NexusMods.Monitor.Shared.Host
 {
     public class PollyUtils
     {
+        private static TimeSpan GetServerWaitDuration(DelegateResult<HttpResponseMessage> response)
+        {
+            var retryAfter = response?.Result?.Headers?.RetryAfter;
+            if (retryAfter == null)
+                return TimeSpan.Zero;
+
+            return retryAfter.Date.HasValue ? retryAfter.Date.Value - DateTime.UtcNow : retryAfter.Delta.GetValueOrDefault(TimeSpan.Zero);
+        }
+
         public static IAsyncPolicy<HttpResponseMessage> PolicySelector(IServiceProvider sp, HttpRequestMessage request)
         {
             var logger = sp.GetRequiredService<ILogger<PollyUtils>>();
-            return Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+
+            return Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
                 .OrTransientHttpError()
                 .Or<SocketException>()
-                .WaitAndRetryAsync(20, _ => TimeSpan.FromSeconds(2), (delegateResult, time) =>
-                {
-                    logger.LogError(delegateResult.Exception, "Exception during HTTP connection. HttpResult {@HttpResult}. Waiting {Time}...", time, delegateResult.Result);
-                });
+                .WaitAndRetryAsync(
+                    retryCount: 20,
+                    sleepDurationProvider: (i, result, context) =>
+                    {
+                        var clientWaitDuration = TimeSpan.FromSeconds(2);
+                        var serverWaitDuration = GetServerWaitDuration(result);
+                        var waitDuration = Math.Max(clientWaitDuration.TotalMilliseconds, serverWaitDuration.TotalMilliseconds);
+                        return TimeSpan.FromMilliseconds(waitDuration);
+                    },
+                    onRetryAsync: async (result, timeSpan, retryCount, context) =>
+                    {
+                        logger.LogError(result.Exception, "Exception during HTTP connection. HttpResult {@HttpResult}. Retry count {RetryCount}. Waiting {Time}...", result.Result, retryCount, timeSpan);
+                    });
         }
     }
 }
