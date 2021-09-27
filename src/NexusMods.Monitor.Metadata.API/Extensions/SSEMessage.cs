f6b5e8e0050dd4bcb18e126c696ba06a12d68b84
aragas <va_mikhaylov@head-point.ru>
2021-09-27 16:04:31 +0300

    Deleted nused

--- src/NexusMods.Monitor.Metadata.API/Extensions/SSEMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace NexusMods.Monitor.Metadata.API.Extensions
-{
-    public record SSEMessage(string? Id = null, string? Event = null, IAsyncEnumerable<string>? Data = null, int? Retry = null)
-    {
-        public bool IsEmpty => Id is null && Event is null && Data is null;
-    }
-}
