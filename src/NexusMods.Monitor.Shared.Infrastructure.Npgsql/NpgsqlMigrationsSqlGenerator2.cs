97d4bbdc3dbeb07f10bb16478aece57b7eae734f
Aragas <personal@aragas.org>
2021-10-06 21:34:43 +0300

    Fix

--- src/NexusMods.Monitor.Shared.Infrastructure.Npgsql/NpgsqlMigrationsSqlGenerator2.cs
+++ src/NexusMods.Monitor.Shared.Infrastructure.Npgsql/NpgsqlMigrationsSqlGenerator2.cs
@@ -12,30 +12,31 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 
-namespace NexusMods.Monitor.Shared.Infrastructure.Npgsql;
-
-[SuppressMessage("Usage", "EF1001", Justification = "Bootleg")]
-public class NpgsqlMigrationsSqlGenerator2 : NpgsqlMigrationsSqlGenerator
+namespace NexusMods.Monitor.Shared.Infrastructure.Npgsql
 {
-    public NpgsqlMigrationsSqlGenerator2(MigrationsSqlGeneratorDependencies dependencies, INpgsqlOptions npgsqlOptions) : base(dependencies, npgsqlOptions) { }
-
-    protected override void Generate(InsertDataOperation operation, IModel? model, MigrationCommandListBuilder builder, bool terminate = true)
+    [SuppressMessage("Usage", "EF1001", Justification = "Bootleg")]
+    public class NpgsqlMigrationsSqlGenerator2 : NpgsqlMigrationsSqlGenerator
     {
-        var sqlBuilder = new StringBuilder();
-        foreach (var modificationCommand in GenerateModificationCommands(operation, model))
-        {
-            var overridingSystemValue = modificationCommand.ColumnModifications.Any(m => m.Property?.GetValueGenerationStrategy() == NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
-            var subSqlBuilder = new StringBuilder();
-            ((NpgsqlUpdateSqlGenerator) Dependencies.UpdateSqlGenerator).AppendInsertOperation(subSqlBuilder, modificationCommand, 0, overridingSystemValue);
-            subSqlBuilder.Replace(";", " ON CONFLICT DO NOTHING;");
-            sqlBuilder.Append(subSqlBuilder);
-        }
+        public NpgsqlMigrationsSqlGenerator2(MigrationsSqlGeneratorDependencies dependencies, INpgsqlOptions npgsqlOptions) : base(dependencies, npgsqlOptions) { }
 
-        builder.Append(sqlBuilder.ToString());
-
-        if (terminate)
+        protected override void Generate(InsertDataOperation operation, IModel? model, MigrationCommandListBuilder builder, bool terminate = true)
         {
-            builder.EndCommand();
+            var sqlBuilder = new StringBuilder();
+            foreach (var modificationCommand in GenerateModificationCommands(operation, model))
+            {
+                var overridingSystemValue = modificationCommand.ColumnModifications.Any(m => m.Property?.GetValueGenerationStrategy() == NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
+                var subSqlBuilder = new StringBuilder();
+                ((NpgsqlUpdateSqlGenerator) Dependencies.UpdateSqlGenerator).AppendInsertOperation(subSqlBuilder, modificationCommand, 0, overridingSystemValue);
+                subSqlBuilder.Replace(";", " ON CONFLICT DO NOTHING;");
+                sqlBuilder.Append(subSqlBuilder);
+            }
+
+            builder.Append(sqlBuilder.ToString());
+
+            if (terminate)
+            {
+                builder.EndCommand();
+            }
         }
     }
 }
--- src/NexusMods.Monitor.Shared.Infrastructure.Npgsql/NpgsqlMigrationsSqlGeneratorPath.cs
+++ src/NexusMods.Monitor.Shared.Infrastructure.Npgsql/NpgsqlMigrationsSqlGeneratorPath.cs
@@ -10,53 +10,54 @@ using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
-namespace NexusMods.Monitor.Shared.Infrastructure.Npgsql;
-
-public static class NpgsqlMigrationsSqlGeneratorPath
+namespace NexusMods.Monitor.Shared.Infrastructure.Npgsql
 {
-    public static void Patch(Harmony harmony)
+    public static class NpgsqlMigrationsSqlGeneratorPath
     {
-        var generateTableMethod = AccessTools.Method(
-            typeof(NpgsqlMigrationsSqlGenerator),
-            nameof(NpgsqlMigrationsSqlGenerator.Generate),
-            new[] { typeof(CreateTableOperation), typeof(IModel), typeof(MigrationCommandListBuilder), typeof(bool) });
-
-        var generateIndexMethod = AccessTools.Method(
-            typeof(NpgsqlMigrationsSqlGenerator),
-            nameof(NpgsqlMigrationsSqlGenerator.Generate),
-            new[] { typeof(CreateIndexOperation), typeof(IModel), typeof(MigrationCommandListBuilder), typeof(bool) });
-
-        if (generateTableMethod is null || generateIndexMethod is null)
-            return;
-
-        harmony.Patch(
-            original: generateTableMethod,
-            transpiler: new HarmonyMethod(AccessTools.Method(typeof(NpgsqlMigrationsSqlGeneratorPath), nameof(GenerateTableTranspiler))));
-
-        harmony.Patch(
-            original: generateIndexMethod,
-            transpiler: new HarmonyMethod(AccessTools.Method(typeof(NpgsqlMigrationsSqlGeneratorPath), nameof(GenerateIndexTranspiler))));
-    }
-
-    private static IEnumerable<CodeInstruction> GenerateTableTranspiler(IEnumerable<CodeInstruction> instructions)
-    {
-        foreach (var codeInstruction in instructions)
+        public static void Patch(Harmony harmony)
         {
-            if (codeInstruction.opcode == OpCodes.Ldstr && codeInstruction.operand is string str && str.Trim().Equals("table", StringComparison.OrdinalIgnoreCase))
-                codeInstruction.operand = "TABLE IF NOT EXISTS ";
+            var generateTableMethod = AccessTools.Method(
+                typeof(NpgsqlMigrationsSqlGenerator),
+                nameof(NpgsqlMigrationsSqlGenerator.Generate),
+                new[] { typeof(CreateTableOperation), typeof(IModel), typeof(MigrationCommandListBuilder), typeof(bool) });
 
-            yield return codeInstruction;
+            var generateIndexMethod = AccessTools.Method(
+                typeof(NpgsqlMigrationsSqlGenerator),
+                nameof(NpgsqlMigrationsSqlGenerator.Generate),
+                new[] { typeof(CreateIndexOperation), typeof(IModel), typeof(MigrationCommandListBuilder), typeof(bool) });
+
+            if (generateTableMethod is null || generateIndexMethod is null)
+                return;
+
+            harmony.Patch(
+                original: generateTableMethod,
+                transpiler: new HarmonyMethod(AccessTools.Method(typeof(NpgsqlMigrationsSqlGeneratorPath), nameof(GenerateTableTranspiler))));
+
+            harmony.Patch(
+                original: generateIndexMethod,
+                transpiler: new HarmonyMethod(AccessTools.Method(typeof(NpgsqlMigrationsSqlGeneratorPath), nameof(GenerateIndexTranspiler))));
         }
-    }
 
-    private static IEnumerable<CodeInstruction> GenerateIndexTranspiler(IEnumerable<CodeInstruction> instructions)
-    {
-        foreach (var codeInstruction in instructions)
+        private static IEnumerable<CodeInstruction> GenerateTableTranspiler(IEnumerable<CodeInstruction> instructions)
         {
-            if (codeInstruction.opcode == OpCodes.Ldstr && codeInstruction.operand is string str && str.Trim().Equals("index", StringComparison.OrdinalIgnoreCase))
-                codeInstruction.operand = "INDEX IF NOT EXISTS ";
+            foreach (var codeInstruction in instructions)
+            {
+                if (codeInstruction.opcode == OpCodes.Ldstr && codeInstruction.operand is string str && str.Trim().Equals("table", StringComparison.OrdinalIgnoreCase))
+                    codeInstruction.operand = "TABLE IF NOT EXISTS ";
 
-            yield return codeInstruction;
+                yield return codeInstruction;
+            }
+        }
+
+        private static IEnumerable<CodeInstruction> GenerateIndexTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var codeInstruction in instructions)
+            {
+                if (codeInstruction.opcode == OpCodes.Ldstr && codeInstruction.operand is string str && str.Trim().Equals("index", StringComparison.OrdinalIgnoreCase))
+                    codeInstruction.operand = "INDEX IF NOT EXISTS ";
+
+                yield return codeInstruction;
+            }
         }
     }
 }
